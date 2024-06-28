b790b23dbb0fa2886d2f89a50f1cbc863a279412
ulushbey <ulushbey.h@gmail.com>
2024-06-29 02:59:27 +0900

    fix: modify WebSocket Chatting Client final stage

--- apollo/client.ts
+++ apollo/client.ts
@@ -7,6 +7,7 @@ import { onError } from '@apollo/client/link/error';
 import { getJwtToken } from '../libs/auth';
 import { TokenRefreshLink } from 'apollo-link-token-refresh';
 import { sweetErrorAlert } from '../libs/sweetAlert';
+import { socketVar } from './store';
 let apolloClient: ApolloClient<NormalizedCacheObject>;
 
 function getHeaders() {
@@ -32,7 +33,8 @@ const tokenRefreshLink = new TokenRefreshLink({
 class LoggingWebSocket {
 	private socket: WebSocket;
 	constructor(url: string) {
-		this.socket = new WebSocket(url);
+		this.socket = new WebSocket(`${url}?token=${getJwtToken()}`);
+		socketVar(this.socket);
 
 		this.socket.onopen = () => {
 			console.log('WebSocket connection!');
